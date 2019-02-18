2239e766efbe49030245d5f50cb0a41abd71b862
Gabriel Riestra <jesus.riestra@live.com.mx>
2019-02-18 08:59:27 -0700

    primera terminada
    3 imagenes con texto

--- app/src/main/java/mx/com/gabinriestrin/actividadparcial/MainActivity.java
+++ app/src/main/java/mx/com/gabinriestrin/actividadparcial/MainActivity.java
@@ -6,11 +6,28 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    TextView txtTextView, txtTextView2, txtTextView3;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        //relación del textview de la interfaz con la variable que vamos a trabajar
+        // se hace cast para que todo lo que venga del findViewid se convierta en TextView
+
+        txtTextView = (TextView) findViewById(R.id.textView);
+        txtTextView2 = (TextView) findViewById(R.id.textView2);
+        txtTextView3 = (TextView) findViewById(R.id.textView3);
+
+        //cambiar texto
+        txtTextView.setText("Battle Angel");
+        txtTextView2.setText("Como entrenar a tu dragón");
+        txtTextView3.setText("Mis huellas a casa");
 
 
     }
