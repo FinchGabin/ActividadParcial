1eb961d3568e59041dbba600c71b1a18b5123639
Gabriel Riestra <jesus.riestra@live.com.mx>
2019-02-20 08:42:14 -0700

    Examen Parcial
    Ya terminado

--- app/src/main/java/mx/com/gabinriestrin/actividadparcial/MainActivity.java
+++ app/src/main/java/mx/com/gabinriestrin/actividadparcial/MainActivity.java
@@ -7,7 +7,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
 
-    TextView txtTextView, txtTextView2, txtTextView3;
+    TextView txtTextView, txtTextView2, txtTextView3, txtDescripcion1, txtDescripcion2, txtDescripcion3;
 
 
 
@@ -23,11 +23,20 @@ public class MainActivity extends AppCompatActivity {
         txtTextView = (TextView) findViewById(R.id.textView);
         txtTextView2 = (TextView) findViewById(R.id.textView2);
         txtTextView3 = (TextView) findViewById(R.id.textView3);
+        txtDescripcion1 = (TextView) findViewById(R.id.textView4);
+        txtDescripcion2 = (TextView) findViewById(R.id.textView5);
+        txtDescripcion3 = (TextView) findViewById(R.id.textView6);
+
 
         //cambiar texto
         txtTextView.setText("Battle Angel");
         txtTextView2.setText("Como entrenar a tu dragón");
         txtTextView3.setText("Mis huellas a casa");
+        txtDescripcion1.setText("Un robot ojon que pelea");
+        txtDescripcion2.setText("Otro mata dragones");
+        txtDescripcion3.setText("Una perrita volviendo a casa");
+
+
 
 
     }
